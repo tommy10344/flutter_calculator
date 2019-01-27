c8046babf8ce2277f41e3f87c639acc5472bc9f7
Hiroaki Tomiyoshi <htomiyosi@gmail.com>
2019-01-28 01:46:49 +0900

    Initial

---
+++ lib/main.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.purple,
+      ),
+      home: MyHomePage(title: 'Flutter Calculator'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+//  int _counter = 0;
+  double _inputValue = 0;
+  double _resultValue = 0;
+  bool _showingResult = false;
+
+//  void _incrementCounter() {
+//    setState(() {
+//      // This call to setState tells the Flutter framework that something has
+//      // changed in this State, which causes it to rerun the build method below
+//      // so that the display can reflect the updated values. If we changed
+//      // _counter without calling setState(), then the build method would not be
+//      // called again, and so nothing would appear to happen.
+//      _counter++;
+//    });
+//  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(32.0),
+        child: Column(
+          children: <Widget>[
+            Text('123'),
+            buttonArea(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buttonArea() {
+    return Expanded(
+      child: Row(
+        children: <Widget>[
+          buttonRow([
+            button("1"),
+            button("2"),
+            button("3"),
+          ]),
+          buttonRow([
+            button("4"),
+            button("5"),
+            button("6"),
+          ]),
+          buttonRow([
+            button("7"),
+            button("8"),
+            button("9"),
+          ]),
+        ],
+      ),
+    );
+  }
+
+  Widget buttonRow(List<Widget> children) {
+    return Expanded(
+      child: Column(children: children),
+    );
+  }
+
+  Widget button(String text) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child:
+          MaterialButton(child: Text(text), color: Theme.of(context).primaryColor),
+    );
+  }
+}
